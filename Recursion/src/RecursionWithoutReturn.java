32f71892a15fe4c7dee5fe6e2096cbe02d83a01f
Manoj.Langote <manoj.langote@dxc.com>
2022-06-23 12:14:21 +0530

    "Recursion without return type"

--- Recursion/src/RecursionWithoutReturn.java
+++ Recursion/src/RecursionWithoutReturn.java
@@ -1,15 +1,1 @@
-public class RecursionWithoutReturn {
 
-    public static void main(String[] args) {
-            print(5);
-    }
-    static void print(int n){
-        if(n<=0){
-            return;
-        }
-        //Print number
-        System.out.println(n);
-        //Recursive fun call without return statement because function return nothing
-        print(--n);
-    }
-}
