aef25e33d0a1db173162c5d4e4a830767c90b7d5
Manoj.Langote <manoj.langote@dxc.com>
2022-06-23 11:56:45 +0530

    "Binary Search Using Recursion"

---
+++ Recursion/src/RecursionBS.java
@@ -0,0 +1,24 @@
+public class RecursionBS {
+    public static void main(String[] args) {
+
+        int []arr={1,2,3,4,5,6,7,8,9,10};
+        int target=7;
+        System.out.println("Index: "+BS(arr,target,0,arr.length-1));
+    }
+    static int BS(int arr[], int target, int start, int end){
+        //Base case
+        if(start>end){
+            return -1;
+        }
+        int mid=start+(end-start)/2;
+        //Body Part which is temporary for perticular function call
+        if(arr[mid]==target){
+            return mid;
+        }else if(arr[mid]>target){
+            end=mid-1;
+        }else{
+            start=mid+1;
+        }
+        return BS(arr,target,start,end);
+    }
+}
