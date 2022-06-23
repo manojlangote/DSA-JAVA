69d6db1ed6e8be4172789ad490bb8d97637d9e81
Manoj.Langote <manoj.langote@dxc.com>
2022-06-23 13:23:10 +0530

    Straight_Reverse_BothWay_IMP

--- Recursion/src/Straight_Reverse_BothWay_IMP.java
+++ Recursion/src/Straight_Reverse_BothWay_IMP.java
@@ -7,27 +7,30 @@ public class Straight_Reverse_BothWay_IMP {
         System.out.println();
         funBoth(5);//Print first, call recursive fun till base case and then print again while coming back(Reverse)
     }
-    static void fun(int n){
-        if(n<=0){
+
+    static void fun(int n) {
+        if (n <= 0) {
             return;
         }
         System.out.println(n);
-        fun(n-1);
+        fun(n - 1);
     }
-    static void funRev(int n){
-        if(n==0){
+
+    static void funRev(int n) {
+        if (n == 0) {
             return;
         }
         //Try to go at last and then print while coming back
-        funRev(n-1);
+        funRev(n - 1);
         System.out.println(n);
     }
-    static void funBoth(int n){
-        if(n<=0){
+
+    static void funBoth(int n) {
+        if (n <= 0) {
             return;
         }
         System.out.println(n);
-        funBoth(n-1);
+        funBoth(n - 1);
         System.out.println(n);
     }
 
